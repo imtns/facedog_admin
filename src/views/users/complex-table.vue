00870aa170c743dae31c47bab77635c792dbed1b
imtns <imtns@sina.com>
2020-04-27 10:31:20 +0800

    update

--- src/views/users/complex-table.vue
+++ src/views/users/complex-table.vue
@@ -1,83 +1,161 @@
 <template>
     <div class="app-container">
         <div class="filter-container">
-            <el-input v-model="listQuery.search" placeholder="用户昵称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-            <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+            <el-input v-model="listQuery.search"
+                      placeholder="用户昵称"
+                      style="width: 200px;"
+                      class="filter-item"
+                      @keyup.enter.native="handleFilter" />
+            <el-button v-waves
+                       class="filter-item"
+                       type="primary"
+                       icon="el-icon-search"
+                       @click="handleFilter">
                 Search
             </el-button>
         </div>
 
-        <el-table :key="tableKey" v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%;">
-            <el-table-column label="UID" align="center" width="80">
+        <el-table :key="tableKey"
+                  v-loading="listLoading"
+                  :data="list"
+                  border
+                  fit
+                  highlight-current-row
+                  style="width: 100%;">
+            <el-table-column label="UID"
+                             align="center"
+                             width="80">
                 <template slot-scope="scope">
-                    <router-link :to="'/users/'+scope.row._id" class="link-type">
+                    <router-link :to="'/users/'+scope.row._id"
+                                 class="link-type">
                         <span>{{ scope.row.ID }}</span>
                     </router-link>
                     <!--<el-button type="text" @click="userDetails(scope.row._id)">{{ scope.row.ID }}</el-button>-->
                 </template>
             </el-table-column>
-            <el-table-column label="邀请码" width="110px" align="center">
+            <el-table-column label="邀请码"
+                             width="110px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.be_guided }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="注册时间" width="180px" align="center">
+            <el-table-column label="注册时间"
+                             width="180px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.login }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="最后登录时间" width="180px" align="center">
+            <el-table-column label="最后登录时间"
+                             width="180px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.last }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="昵称" width="180px" align="center">
+            <el-table-column label="昵称"
+                             width="180px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.nick_name }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="性别" width="90px" align="center">
+            <el-table-column label="性别"
+                             width="90px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.gender| genderFilter }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="当前状态" width="110px" align="center">
+            <el-table-column label="当前状态"
+                             width="110px"
+                             align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.progress|statusFilter }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="设置评级" width="140px" align="center">
+            <el-table-column label="图片"
+                             width="80px"
+                             align="center">
                 <template slot-scope="scope">
-                    <el-select v-model="scope.row.level" placeholder="设置评级" @change="levelChange(scope.row.level,scope.row._id)">
-                        <el-option v-for="item in level" :key="item" :label="item" :value="item">
+                    <el-button type="text"
+                               @click="showImage(scope.row.avatar)">查看头像</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="设置评级"
+                             width="140px"
+                             align="center">
+                <template slot-scope="scope">
+                    <el-select v-model="scope.row.level"
+                               placeholder="设置评级"
+                               @change="levelChange(scope.row.level,scope.row._id)">
+                        <el-option v-for="item in level"
+                                   :key="item"
+                                   :label="item"
+                                   :value="item">
                         </el-option>
                     </el-select>
                 </template>
             </el-table-column>
-            <el-table-column label="真人认证" width="110px" align="center">
+            <el-table-column label="真人认证"
+                             width="110px"
+                             align="center">
                 <template slot-scope="scope">
-                    <el-select v-model="scope.row.video_audit" value-key="key" placeholder="设置评级" @change="auditChange(scope.row.video_audit,scope.row._id)">
-                        <el-option v-for="item in audit" :key="item.name" :label="item.name" :value="item.key">
+                    <el-select v-model="scope.row.video_audit"
+                               value-key="key"
+                               placeholder="设置评级"
+                               @change="auditChange(scope.row.video_audit,scope.row._id)">
+                        <el-option v-for="item in audit"
+                                   :key="item.name"
+                                   :label="item.name"
+                                   :value="item.key">
                         </el-option>
                     </el-select>
                 </template>
             </el-table-column>
-            <el-table-column label="操作" align="center" width="180" class-name="small-padding fixed-width">
+            <el-table-column label="操作"
+                             align="center"
+                             width="180"
+                             class-name="small-padding fixed-width">
                 <template slot-scope="{row}">
-                    <el-button size="mini" type="danger" @click="showDelete(row._id)">
+                    <el-button size="mini"
+                               type="danger"
+                               @click="showDelete(row._id)">
                         删除
                     </el-button>
                 </template>
             </el-table-column>
         </el-table>
 
-        <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
-        <el-dialog v-el-drag-dialog width="30%" :visible.sync="dialogTooltip" title="警告" @dragDialog="handleDrag">
+        <pagination v-show="total>0"
+                    :total="total"
+                    :page.sync="listQuery.page"
+                    :limit.sync="listQuery.limit"
+                    @pagination="getList" />
+        <el-dialog v-el-drag-dialog
+                   width="30%"
+                   :visible.sync="dialogTooltip"
+                   title="警告"
+                   @dragDialog="handleDrag">
             <div class="text-center">如果删除该用户则该用户在本平台上发布的数据以及操作将被全部抹掉，确定删除该用户？</div>
-            <div class="text-center" style="padding-top:20px">
-                <el-button type="danger" @click="dialogTooltip=false" style="margin-right:50px">取消</el-button>
-                <el-button type="primary" @click="deleteUser()">确定</el-button>
+            <div class="text-center"
+                 style="padding-top:20px">
+                <el-button type="danger"
+                           @click="dialogTooltip=false"
+                           style="margin-right:50px">取消</el-button>
+                <el-button type="primary"
+                           @click="deleteUser()">确定</el-button>
             </div>
+        </el-dialog>
+        <el-dialog v-el-drag-dialog
+                   :visible.sync="dialogTableVisible"
+                   :title="dialogTitle"
+                   @dragDialog="handleDrag">
+            <img v-if="showWindows.type==='image'"
+                 :src="showWindows.url"
+                 alt=""
+                 style="width: 400px;height:400px">
         </el-dialog>
     </div>
 </template>
@@ -129,6 +207,7 @@ export default {
       tableKey: 0,
       list: null,
       level: ["N", "R", "SR", "SSR"],
+      dialogTableVisible: false,
       audit: [
         {
           key: "auditing",
@@ -147,7 +226,8 @@ export default {
       dialogTooltip: false,
       delete_id: "",
       listLoading: true,
-      dialogTitle:"",
+      showWindows: {},
+      dialogTitle: "",
       listQuery: {
         page: 1,
         limit: 20,
@@ -159,6 +239,11 @@ export default {
     this.getList();
   },
   methods: {
+    showImage(url) {
+      this.dialogTitle = "用户图片";
+      this.showWindows = { type: "image", url: url };
+      this.dialogTableVisible = true;
+    },
     levelChange(level, id) {
       console.log(level, id);
       set_level(level, id).then(() => {
